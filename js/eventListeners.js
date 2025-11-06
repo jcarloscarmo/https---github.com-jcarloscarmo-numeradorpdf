396f28ed80196411ed0b0168dbc35f27eb76ba3f
José Carlos <jcarllos.carmo@gmail.com>
2025-11-05 21:20:43 -0300

    new features

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -8,3 +8,13 @@ folderInput.addEventListener('change', handleFileSelect);
 positionRadios.forEach(radio => radio.addEventListener('change', updateStampPreview));
 
 generateBtn.addEventListener('click', generateAndDownloadFiles);
+
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', function(event) {
+        const href = this.getAttribute('href');
+        if (href && href !== '#') {
+            event.preventDefault();
+            window.location.href = href;
+        }
+    });
+});
